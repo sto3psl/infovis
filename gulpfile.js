7a88c97cb67db50a5b1f479974429ca970dca133
sto3psl <stoepsl93@gmx.de>
2015-05-18 00:35:41 +0200

    package.json & gulpfile aufgeräumt , Let's use some MAGIC

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,9 @@
-var postcss = require('gulp-postcss')
 var gulp = require('gulp')
-var rename = require('gulp-rename')
-// var autoprefixer = require('autoprefixer-core')
-// var mqpacker = require('css-mqpacker')
-var csswring = require('csswring')
-var cssnext = require('cssnext')
-// var cssimport = require('postcss-import')
 var del = require('del')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
-
-gulp.task('css', function () {
-  var processors = [
-    cssnext(),
-    csswring
-  ]
-  return gulp.src('./style/style.css')
-    .pipe(postcss(processors))
-    .pipe(rename('app.build.css'))
-    .pipe(gulp.dest('./build'))
-})
 
 gulp.task('clean', function () {
   del(['build/*.js', 'build/*.css'], function (err, deletedFiles) {
     if (err) throw console.log('Error')
     console.log('Files deleted:', deletedFiles.join(', '))
   })
-})
-
-gulp.task('build-js', function () {
-  return browserify('./index.js', { debug: true })
-    .transform(require('jadeify'))
-    .bundle()
-    .pipe(source('app.build.js'))
-    .pipe(gulp.dest('./build/'))
 })
